777059d9e79f80eb4df1e4e54d1db1d48d4658b8
Anton Mamontov <anton_mamont@bk.ru>
2020-05-13 10:12:02 +0300

    Updates

---
+++ precache-manifest.2f43e1effed0d2578d40cc69bcdba7a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bec8b45355103d25cae3d18804fe27aa",
+    "url": "/tohaheleneto/index.html"
+  },
+  {
+    "revision": "4451f065d987aa600515",
+    "url": "/tohaheleneto/static/css/main.9672290d.chunk.css"
+  },
+  {
+    "revision": "0794a09813244e110cd3",
+    "url": "/tohaheleneto/static/js/2.6a548205.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tohaheleneto/static/js/2.6a548205.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4451f065d987aa600515",
+    "url": "/tohaheleneto/static/js/main.55168c6b.chunk.js"
+  },
+  {
+    "revision": "ea2b6f97b1fbd3c266d7",
+    "url": "/tohaheleneto/static/js/runtime-main.cac2b716.js"
+  }
+]);
--- src/components/Main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export function Main() {
-    return (
-        <div>
-            <a href="cat">Диета кота</a>
-        </div>
-    )
-}
